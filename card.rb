f935584ab221d59e145e1265e0f367deb02df701
Tim Arbaev <timbrk@gmail.com>
2018-12-05 14:48:55 +0300

    add Card class, Deck class

---
+++ card.rb
@@ -0,0 +1,21 @@
+class Card
+  def initialize(number, suit)
+    @number = number
+    @suit = suit
+    # TODO: сделать валидацию
+  end
+
+  def value
+    return 10 if @number > 10
+
+    @number
+  end
+
+  def show
+    card_names = { 1 => 'A', 11 => 'J', 12 => 'Q', 13 => 'K' }
+    suit_names = { clubs: '♣', diamonds: '♦', hearts: '♥', spades: '♠' }
+    name = card_names[@number] || @number
+    suit = suit_names[@suit]
+    "#{name}#{suit}"
+  end
+end
---
+++ deck.rb
@@ -0,0 +1,16 @@
+class Deck
+  SUITS = %i[clubs diamonds hearts spades].freeze
+  attr_reader :deck
+
+  def initialize
+    @deck = make_deck.shuffle
+  end
+
+  def make_deck
+    (1..13).to_a.product(SUITS).map { |number, suit| Card.new(number, suit) }
+  end
+
+  def deal_card
+    @deck.pop
+  end
+end
