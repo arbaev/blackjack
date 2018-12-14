52fd4375425c3be61f1d1c4fc5369e8abffab464
Tim Arbaev <timbrk@gmail.com>
2018-12-14 07:27:08 +0300

    universal move method

--- round.rb
+++ round.rb
@@ -6,53 +6,59 @@ class Round
   attr_reader :result
 
   def initialize(player1, player2)
-    @player1 = player1
-    @player2 = player2
+    @player = player1
+    @dealer = player2
     @result = nil
     @deck = Deck.new
   end
 
   def play
-    deal_cards(@player1, 2)
-    deal_cards(@player2, 2)
-    show_player_cards(@player1, :open)
-    show_player_cards(@player2, :hidden)
+    setup_round
     moves
+    player_cards(@dealer)
     @result = who_wins
-    show_player_cards(@player1)
-    show_player_cards(@player2)
   end
 
   private
 
+  def setup_round
+    deal_cards(@player, 2)
+    deal_cards(@dealer, 2)
+    player_cards(@player, :open)
+    player_cards(@dealer, :hidden)
+  end
+
   def moves
-    decision = @player1.decision
-    return if decision == :open
-    @player1.hand.take_card(@deck.deal_card) if decision == :take
-    show_player_cards(@player1, :open)
+    return if move(@player) == :open
+    return if move(@dealer) == :open
 
-    decision = @player2.decision
-    return if decision == :open
-    @player2.hand.take_card(@deck.deal_card) if decision == :take
-    Interface.show_decision(@player2.name, decision)
-    show_player_cards(@player2, :hidden)
+    move(@player)
+  end
 
-    decision = @player1.decision
-    @player1.hand.take_card(@deck.deal_card) if decision == :take
+  def move(player)
+    return :open if player.hand.cards.size == 3
+
+    decision = player.decision
+    return :open if decision == :open
+
+    deal_cards(player, 1) if decision == :take
+    Interface.show_decision(player.name, decision)
+    type = player.class == Dealer ? :hidden : :show
+    player_cards(player, type)
   end
 
   def who_wins
     # Выигрывает игрок, у которого сумма очков ближе к 21
     # Если у игрока сумма очков больше 21, то он проиграл
     # Если сумма очков у игрока и дилера одинаковая, то объявляется ничья
-    score1 = @player1.hand.score
-    score2 = @player2.hand.score
+    score1 = @player.hand.score
+    score2 = @dealer.hand.score
     return :both_lost if [score1, score2].min > BLACKJACK
 
-    return @player1 if score2 > BLACKJACK
-    return @player2 if score1 > BLACKJACK
-    return @player1 if BLACKJACK - score1 < BLACKJACK - score2
-    return @player2 if BLACKJACK - score1 > BLACKJACK - score2
+    return @player if score2 > BLACKJACK
+    return @dealer if score1 > BLACKJACK
+    return @player if BLACKJACK - score1 < BLACKJACK - score2
+    return @dealer if BLACKJACK - score1 > BLACKJACK - score2
 
     :draw
   end
@@ -61,11 +67,11 @@ class Round
     quantity.times { player.hand.take_card(@deck.deal_card) }
   end
 
-  def show_player_cards(player, type = :open)
-    if type == :open
-      Interface.show_cards(player.name, player.hand.show, player.hand.score)
-    else
+  def player_cards(player, type = :show)
+    if type == :hidden
       Interface.show_cards(player.name, player.hand.show_hidden, player.hand.score_hidden)
+    else
+      Interface.show_cards(player.name, player.hand.show, player.hand.score)
     end
   end
 end
